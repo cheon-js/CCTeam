5964f5fb41961349525640a51327082f754e0199
songeon <songeon7337@naver.com>
2020-11-18 21:56:38 +0900

    이제 될듯

--- app/src/main/java/com/example/ccteam/HomeActivity.java
+++ app/src/main/java/com/example/ccteam/HomeActivity.java
@@ -1,4 +1,13 @@
 package com.example.ccteam;
 
-public class HomeActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
 }
