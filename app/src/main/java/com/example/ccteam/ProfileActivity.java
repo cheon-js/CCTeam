deee7bc8d718d9b6ba7ca9d01a0661d85a7bd714
songeon <songeon7337@naver.com>
2020-11-18 20:50:56 +0900

    로그인후 화면전환 이메일이랑 프로필 정보 가져오긴 하는것같은데 출력이왜안될까?

--- app/src/main/java/com/example/ccteam/ProfileActivity.java
+++ app/src/main/java/com/example/ccteam/ProfileActivity.java
@@ -3,77 +3,52 @@ package com.example.ccteam;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.squareup.picasso.Picasso;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
-
+public class ProfileActivity extends AppCompatActivity {
     public static final String GOOGLE_ACCOUNT = "google_account";
-    private TextView profileName, profileEmail;
-    private ImageView profileImage;
-    private GoogleSignInAccount mGoogleSignInAccount;
-    private GoogleSignInClient mGoogleSignInClient;
+    TextView name, mail;
+    Button gocontinue, logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-        // Build a GoogleSignInClient with the options specified by gso.
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        gocontinue = findViewById(R.id.next_continue);
+        logout = findViewById(R.id.log_out);
+        name = findViewById(R.id.profile_text);
+        mail = findViewById(R.id.profile_email);
 
-        profileName = findViewById(R.id.profile_text);
-        profileEmail = findViewById(R.id.profile_email);
-        profileImage = findViewById(R.id.profile_image);
-
-        //findViewById(R.id.sign_out).setOnClickListener(this);
-
-        mGoogleSignInAccount = getIntent().getParcelableExtra(GOOGLE_ACCOUNT);
-
-        setDataOnView();
-    }
-
-    private void setDataOnView() {
-        Picasso.get().load(mGoogleSignInAccount.getPhotoUrl()).centerInside().fit().into(profileImage);
-        profileName.setText(mGoogleSignInAccount.getDisplayName());
-        profileEmail.setText(mGoogleSignInAccount.getEmail());
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            // ...
-            case R.id.next_continue:
-                next_continue();
-                break;
-            // ...
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(signInAccount != null){
+            name.setText(signInAccount.getDisplayName());
+            mail.setText(signInAccount.getEmail());
         }
-    }
 
-
-    private void next_continue() {
-        mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+        gocontinue.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                //On Succesfull signout we navigate the user back to LoginActivity
-                Intent intent=new Intent(ProfileActivity.this,HomeActivity.class);
-                //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
             }
         });
     }
 }
+
