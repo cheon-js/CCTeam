603745ecac7f7b08fd670afc88d2692b2633dcd5
songeon <songeon7337@naver.com>
2020-11-27 02:57:19 +0900

    아몰랑 푸싱~ 채팅

--- app/src/main/java/com/example/ccteam/ChatActivity.java
+++ app/src/main/java/com/example/ccteam/ChatActivity.java
@@ -1,8 +1,84 @@
 package com.example.ccteam;
 
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class ChatActivity extends AppCompatActivity {
 
+    EditText et;
+    ListView listView;
+
+    ArrayList<MessageItem> messageItems=new ArrayList<>();
+    ChatAdapter adapter;
+
+    //Firebase Database 관리 객체참조변수
+    FirebaseDatabase firebaseDatabase;
+
+    //'chat'노드의 참조객체 참조변수
+    DatabaseReference chatRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatting);
+
+        //제목줄 제목글시를 닉네임으로(또는 채팅방)
+        getSupportActionBar().setTitle(G.nickName);
+
+        et=findViewById(R.id.et);
+        listView=findViewById(R.id.listview);
+        adapter=new ChatAdapter(messageItems,getLayoutInflater());
+        listView.setAdapter(adapter);
+
+        //Firebase DB관리 객체와 'caht'노드 참조객체 얻어오기
+        firebaseDatabase= FirebaseDatabase.getInstance();
+        chatRef= firebaseDatabase.getReference("chat");
+
+
+        //firebaseDB에서 채팅 메세지들 실시간 읽어오기..
+
+    }
+
+    public void clickSend(View view) {
+
+        //firebase DB에 저장할 값들( 닉네임, 메세지, 프로필 이미지URL, 시간)
+        String nickName= G.nickName;
+        String message= et.getText().toString();
+        String pofileUrl= G.porfileUrl;
+
+        //메세지 작성 시간 문자열로..
+        Calendar calendar= Calendar.getInstance(); //현재 시간을 가지고 있는 객체
+        String time=calendar.get(Calendar.HOUR_OF_DAY)+":"+calendar.get(Calendar.MINUTE); //14:16
+
+        //firebase DB에 저장할 값(MessageItem객체) 설정
+        MessageItem messageItem= new MessageItem(nickName,message,time,pofileUrl);
+        //'char'노드에 MessageItem객체를 통해
+        chatRef.push().setValue(messageItem);
+
+        //EditText에 있는 글씨 지우기
+        et.setText("");
+
+        //소프트키패드를 안보이도록..
+        InputMethodManager imm=(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+
+        //처음 시작할때 EditText가 다른 뷰들보다 우선시 되어 포커스를 받아 버림.
+        //즉, 시작부터 소프트 키패드가 올라와 있음.
+
+        //그게 싫으면...다른 뷰가 포커스를 가지도록
+        //즉, EditText를 감싼 Layout에게 포커스를 가지도록 속성을 추가!![[XML에]
+    }
 }
--- app/src/main/java/com/example/ccteam/ChatProfile.java
+++ app/src/main/java/com/example/ccteam/ChatProfile.java
@@ -1,23 +1,38 @@
 package com.example.ccteam;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ChatProfile extends AppCompatActivity {
     TextView etName;
     ImageView ivProfile;
     Uri personPhoto;
+    Button button;
+    boolean isFirst= true; //앱을 처음 실행한 것인가?
+    boolean isChanged= false; //프로필을 변경한 적이 있는가?
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,15 @@ public class ChatProfile extends AppCompatActivity {
 
         etName = findViewById(R.id.et_name);
         ivProfile = findViewById(R.id.iv_profile);
+        button = findViewById(R.id.chatbutt);
+
+        if(G.nickName!=null){
+            etName.setText(G.nickName);
+            Picasso.get().load(G.porfileUrl).into(ivProfile);
+
+            //처음이 아니다, 즉, 이미 접속한 적이 있다.
+            isFirst=false;
+        }
 
         GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
         if (signInAccount != null) {
@@ -42,6 +66,21 @@ public class ChatProfile extends AppCompatActivity {
                 startActivityForResult(intent,10);
             }
         });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isChanged && !isFirst){
+                    //ChatActivity로 전환
+                    Intent intent= new Intent(getApplicationContext(),ChatActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    //1. save작업
+                    saveData();
+                }
+            }
+        });
     }
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
         super.onActivityResult(requestCode, resultCode, data);
@@ -56,16 +95,70 @@ public class ChatProfile extends AppCompatActivity {
 
                     //Picasso 라이브러리는 퍼미션 없어도 됨.
                     Picasso.get().load(personPhoto).into(ivProfile);
+                    isChanged=true;
                 }
                 break;
         }
     }
-
     public void clickBtn(View view) {
-        //1. save작업
-            saveData();
-        //2. ChatActivity로 전환
+
+        //바꾼것도 없고, 처음 접속도 아니고..
+
     }
     void saveData() {
+        G.nickName= etName.getText().toString();
+
+        //이미지를 선택하지 않았을 수도 있으므로
+        if(personPhoto==null) return;
+
+        //Firebase storage에 이미지 저장하기 위해 파일명 만들기(날짜를 기반으로)
+        SimpleDateFormat sdf= new SimpleDateFormat("yyyyMMddhhmmss"); //20191024111224
+        String fileName= sdf.format(new Date())+".png";
+
+        //Firebase storage에 저장하기
+        FirebaseStorage firebaseStorage= FirebaseStorage.getInstance();
+        final StorageReference imgRef= firebaseStorage.getReference("profileImages/"+fileName);
+
+        //파일 업로드
+        UploadTask uploadTask=imgRef.putFile(personPhoto);
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                //이미지 업로드가 성공되었으므로...
+                //곧바로 firebase storage의 이미지 파일 다운로드 URL을 얻어오기
+                imgRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        //파라미터로 firebase의 저장소에 저장되어 있는
+                        //이미지에 대한 다운로드 주소(URL)을 문자열로 얻어오기
+                        G.porfileUrl= uri.toString();
+                        Toast.makeText(ChatProfile.this, "프로필 저장 완료", Toast.LENGTH_SHORT).show();
+
+                        //1. Firebase Database에 nickName, profileUrl을 저장
+                        //firebase DB관리자 객체 소환
+                        FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
+                        //'profiles'라는 이름의 자식 노드 참조 객체 얻어오기
+                        DatabaseReference profileRef= firebaseDatabase.getReference("profiles");
+
+                        //닉네임을 key 식별자로 하고 프로필 이미지의 주소를 값으로 저장
+                        profileRef.child(G.nickName).setValue(G.porfileUrl);
+
+                        //2. 내 phone에 nickName, profileUrl을 저장
+                        SharedPreferences preferences= getSharedPreferences("account",MODE_PRIVATE);
+                        SharedPreferences.Editor editor=preferences.edit();
+
+                        editor.putString("nickName",G.nickName);
+                        editor.putString("profileUrl", G.porfileUrl);
+
+                        editor.commit();
+                        //저장이 완료되었으니 ChatActivity로 전환
+                        Intent intent= new Intent(getApplicationContext(),ChatActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                    }
+                });
+            }
+        });
     }
 }
