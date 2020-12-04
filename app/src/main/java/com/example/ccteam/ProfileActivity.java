21ce819b0e075ac19f28c1a849fa06f4cee202ec
gyu001004 <gyu001004@naver.com>
2020-12-04 23:07:42 +0900

    메인메뉴+프로필

--- app/src/main/java/com/example/ccteam/ProfileActivity.java
+++ app/src/main/java/com/example/ccteam/ProfileActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,7 +19,7 @@ public class ProfileActivity extends AppCompatActivity {
     TextView name, mail;
     Button gocontinue, logout;
     String email, gname;
-
+    ImageView profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +30,13 @@ public class ProfileActivity extends AppCompatActivity {
         logout = findViewById(R.id.log_out);
         name = findViewById(R.id.profile_text);
         mail = findViewById(R.id.profile_email);
+        profile = findViewById(R.id.profile_image);
         GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
         if(signInAccount != null){
             name.setText(signInAccount.getDisplayName());
             mail.setText(signInAccount.getEmail());
+            String imageUrl = signInAccount.getPhotoUrl().toString(); //v
+            Glide.with(this).load(imageUrl).into(profile); //v
         }
 
         gocontinue.setOnClickListener(new View.OnClickListener() {
