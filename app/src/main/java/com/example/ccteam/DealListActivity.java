544a1893ef8f7e34e180a6c6f1f7dd4b61d6cdc4
jungsuk <cheon0847@naver.com>
2020-12-06 02:37:33 +0900

    중고거래 게시판 기능 완료

--- app/src/main/java/com/example/ccteam/DealListActivity.java
+++ app/src/main/java/com/example/ccteam/DealListActivity.java
@@ -1,12 +1,86 @@
 package com.example.ccteam;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DealListActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Board> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
+
+    Button writebtn;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_deal_write);
+        setContentView(R.layout.activity_board_list);
+
+        recyclerView = findViewById(R.id.recyclerView); // 아디 연결
+        recyclerView.setHasFixedSize(true); // 리사이클러뷰 기존성능 강화
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>();
+
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Dealpostfreelist");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                arrayList.clear();
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Board board = dataSnapshot.getValue(Board.class);
+                    arrayList.add(board);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("MainActivity", String.valueOf(error.toException()));
+
+
+            }
+        });
+
+        adapter = new BoardAdapter(arrayList, this);
+        recyclerView.setAdapter(adapter);
+
+        writebtn = findViewById(R.id.button3);
+        writebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),FreeboardWriteActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/ccteam/DealWriteActivity.java
+++ app/src/main/java/com/example/ccteam/DealWriteActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,8 +22,12 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
+import org.w3c.dom.Text;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class DealWriteActivity extends AppCompatActivity {
     Button input, input_iv;
@@ -31,15 +36,15 @@ public class DealWriteActivity extends AppCompatActivity {
     ImageView image_iv;
     Uri board_iv;
     boolean isChanged= false;
+    Board board1;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_deal_write);
-
+        setContentView(R.layout.activity_freeboard_write);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference userboard = database.getReference("userdealboard");
-        DatabaseReference postdelboard = database.getReference("postdellost");
+        DatabaseReference userboard = database.getReference("userfreeboard");
+        DatabaseReference postfreeboard = database.getReference("Dealpostfreelist");
 
         input = findViewById(R.id.input_button);
         title = findViewById(R.id.board_title);
@@ -50,14 +55,20 @@ public class DealWriteActivity extends AppCompatActivity {
         title.getText().toString();
         content.getText().toString();
 
+
+
         String boradId = "Board_" + System.currentTimeMillis();
         GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+
+        String nameuser = signInAccount.getDisplayName();
+
         input_iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent= new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");
                 startActivityForResult(intent,20);
+                isChanged=true;
             }
         });
 
@@ -65,35 +76,22 @@ public class DealWriteActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(isChanged==true){
-                    postdelboard.child(boradId).child("name").setValue(signInAccount.getDisplayName());
-                    postdelboard.child(boradId).child("Title").setValue(title.getText().toString());
-                    postdelboard.child(boradId).child("Content").setValue(content.getText().toString());
-                    postdelboard.child(boradId).child("ImageUrl").setValue(G.boardUrl);
-                    postdelboard.child(boradId).child("Email").setValue(signInAccount.getEmail().toString());
+                    board1 = new Board(title.getText().toString(), content.getText().toString(), signInAccount.getDisplayName(),G.boardUrl);
+                    postfreeboard.child(boradId).setValue(board1);
+                    userboard.child(nameuser).child(boradId).setValue(board1);
 
-                    userboard.child(boradId).child("name").setValue(signInAccount.getDisplayName());
-                    userboard.child(boradId).child("Title").setValue(title.getText().toString());
-                    userboard.child(boradId).child("Content").setValue(content.getText().toString());
-                    userboard.child(boradId).child("ImageUrl").setValue(G.boardUrl);
-                    userboard.child(boradId).child("Email").setValue(signInAccount.getEmail().toString());
                 }
                 else{
-                    postdelboard.child(boradId).child("name").setValue(signInAccount.getDisplayName());
-                    postdelboard.child(boradId).child("Title").setValue(title.getText().toString());
-                    postdelboard.child(boradId).child("Content").setValue(content.getText().toString());
-                    postdelboard.child(boradId).child("Email").setValue(signInAccount.getEmail().toString());
-
-                    userboard.child(boradId).child("name").setValue(signInAccount.getDisplayName());
-                    userboard.child(boradId).child("Title").setValue(title.getText().toString());
-                    userboard.child(boradId).child("Content").setValue(content.getText().toString());
-                    userboard.child(boradId).child("Email").setValue(signInAccount.getEmail().toString());
+                    board1 = new Board(title.getText().toString(), content.getText().toString(),signInAccount.getDisplayName(),G.boardUrl);
+                    postfreeboard.child(boradId).setValue(board1);
+                    userboard.child(nameuser).child(boradId).setValue(board1);
                 }
-
 
                 Intent intent = new Intent(getApplicationContext(),boardList.class);
                 startActivity(intent);
             }
         });
+
     }
 
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
@@ -103,13 +101,13 @@ public class DealWriteActivity extends AppCompatActivity {
                 if(resultCode==RESULT_OK){
                     board_iv = data.getData();
                     Picasso.get().load(board_iv).into(image_iv);
-                    isChanged=true;
+
                     FirebaseStorage firebaseStorage= FirebaseStorage.getInstance();
 
                     SimpleDateFormat image= new SimpleDateFormat(""+System.currentTimeMillis());
                     String fileName= image.format(new Date())+".png";
 
-                    final StorageReference img= firebaseStorage.getReference("DealboardImage/"+fileName);
+                    final StorageReference img= firebaseStorage.getReference("FreeboardImage/"+fileName);
 
                     UploadTask uploadTask=img.putFile(board_iv);
                     uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
