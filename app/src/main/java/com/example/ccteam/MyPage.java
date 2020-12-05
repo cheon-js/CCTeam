7ab5ae77efb7626f948b619ca4cfbc1dbc4d830a
songe <songeon7337@naver.com>
2020-12-06 06:05:03 +0900

    마이페이지 내가 쓴 게시글 확인 가능

--- app/src/main/java/com/example/ccteam/MyPage.java
+++ app/src/main/java/com/example/ccteam/MyPage.java
@@ -1,16 +1,85 @@
 package com.example.ccteam;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.SignInAccount;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MyPage extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Board> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
+    ImageView iv_google;
+    TextView tv_name, tv_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
 
+        iv_google = findViewById(R.id.myprofile);
+        tv_email = findViewById(R.id.profile_email);
+        tv_name = findViewById(R.id.profile_text);
 
+        recyclerView = findViewById(R.id.alarmlist); // 아디 연결
+        recyclerView.setHasFixedSize(true); // 리사이클러뷰 기존성능 강화
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>();
+
+        database = FirebaseDatabase.getInstance();
+
+
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        String googlename = signInAccount.getDisplayName();
+        databaseReference = database.getReference("userfreeboard").child(googlename);
+        if(signInAccount != null){
+            tv_name.setText(signInAccount.getDisplayName());
+            tv_email.setText(signInAccount.getEmail());
+            String imageUrl = signInAccount.getPhotoUrl().toString();
+            Glide.with(this).load(imageUrl).into(iv_google);
+        }
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                arrayList.clear();
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Board board = dataSnapshot.getValue(Board.class);
+                    arrayList.add(board);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("MainActivity", String.valueOf(error.toException()));
+            }
+        });
+        adapter = new BoardAdapter(arrayList, this);
+        recyclerView.setAdapter(adapter);
     }
 }
